18215238f342c29b5f9a62f78cf901aa3ab536b6
zhoucheng <zhoucheng133@outlook.com>
2025-05-22 20:25:08 +0800

    添加路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,16 @@ const router=createRouter({
       path: "/",
       component: ()=>import("../pages/home.vue")
     },
+    {
+      name: "登录",
+      path: "/login",
+      component: ()=>import("../pages/login.vue")
+    },
+    {
+      name: "注册",
+      path: "/register",
+      component: ()=>import("../pages/register.vue")
+    }
   ]
 })
 
