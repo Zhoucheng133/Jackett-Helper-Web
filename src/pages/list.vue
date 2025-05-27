6ceac0e879ab18b6d8a695ce4df2998a9df518aa
zhoucheng <zhoucheng133@outlook.com>
2025-05-27 20:23:25 +0800

    添加索引列表相关

--- src/pages/list.vue
+++ src/pages/list.vue
@@ -7,15 +7,43 @@
         <Button size="small" icon="pi pi-sliders-h" />
       </ButtonGroup>
     </div>
+    <div class="add_info" v-if="store().indexers.length==0 && !loading">没有添加任何索引</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import TitleBar from '../components/title_bar.vue';
 import { Button, ButtonGroup } from 'primevue';
+import store from '../store';
+import axios from 'axios';
+import { hostname } from '../env/static';
+
+const loading=ref(true);
+
+onMounted(async ()=>{
+  const {data: response}=await axios.get(`${hostname}/api/list/get`, {
+    headers: {
+      token: store().token
+    }
+  })
+  loading.value=false;
+  if(response.ok){
+    store().indexers=response.msg;
+  }
+})
+
 </script>
 
 <style scoped>
+.add_info{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  color: lightgray;
+  user-select: none;
+}
 .tool_bar{
   margin-top: 10px;
   display: flex;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,18 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
+export interface IndexerItem{
+  id: string,
+  url: string,
+}
+
 export default defineStore("index", () => {
   const token=ref("");
 
+  const indexers=ref<IndexerItem[]>([]);
+
   return {
-    token
+    token,
+    indexers,
   };
 })
