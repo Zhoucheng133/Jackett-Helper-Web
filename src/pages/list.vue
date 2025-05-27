2fba70e57fbb700dfcd87da6227b418e2ad7e00d
zhoucheng <zhoucheng133@outlook.com>
2025-05-27 20:13:37 +0800

    添加工具栏

--- src/pages/list.vue
+++ src/pages/list.vue
@@ -1,7 +1,32 @@
 <template>
-  <titleBarCommon/>
+  <TitleBar class="titlebar" />
+  <div class="body">
+    <div class="tool_bar">
+      <ButtonGroup>
+        <Button size="small">添加索引</Button>
+        <Button size="small" icon="pi pi-sliders-h" />
+      </ButtonGroup>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import titleBarCommon from '../components/title_bar_common.vue';
+import TitleBar from '../components/title_bar.vue';
+import { Button, ButtonGroup } from 'primevue';
 </script>
+
+<style scoped>
+.tool_bar{
+  margin-top: 10px;
+  display: flex;
+}
+.body{
+  margin: auto;
+  width: 1000px;
+  padding-bottom: 50px;
+}
+.titlebar{
+  top: 0;
+  position: sticky;
+}
+</style>
