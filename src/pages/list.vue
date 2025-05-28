c28eedcf134c402b34721eff4442af2eb5ba5d8d
zhoucheng <zhoucheng133@outlook.com>
2025-05-28 20:13:19 +0800

    添加列表展示

--- src/pages/list.vue
+++ src/pages/list.vue
@@ -8,6 +8,21 @@
       </ButtonGroup>
     </div>
     <div class="add_info" v-if="store().indexers.length==0 && !loading">没有添加任何索引</div>
+    <DataTable :value="store().indexers" style="user-select: none;">
+      <Column field="name" header="名称"></Column>
+      <Column header="操作">
+        <template #body="slotProps">
+          <div style="width: 100px;">
+            <ButtonGroup>
+              <Button size="small" severity="secondary" @click="showAllDialog(slotProps.data)">全部</Button>
+              <Button size="small" severity="secondary" icon="pi pi-search" />
+            </ButtonGroup>
+          </div>
+        </template>
+      </Column>
+      <Column field="url" header="链接"></Column>
+    </DataTable>
+
     <AddIndexer ref="addIndexerRef"/>
     <AriaConfig ref="ariaConfigRef"/>
   </div>
@@ -18,8 +33,8 @@ import { onMounted, ref } from 'vue';
 import TitleBar from '../components/title_bar.vue';
 import AddIndexer from '../components/add_indexer.vue';
 import AriaConfig from '../components/aria_config.vue';
-import { Button, ButtonGroup } from 'primevue';
-import store from '../store';
+import { Button, ButtonGroup, DataTable, Column } from 'primevue';
+import store, { type IndexerItem } from '../store';
 
 const loading=ref(true);
 const addIndexerRef=ref();
@@ -38,6 +53,10 @@ const setAria=()=>{
   ariaConfigRef.value.showAriaConfig();
 }
 
+const showAllDialog=(data: IndexerItem)=>{
+  // TODO
+}
+
 </script>
 
 <style scoped>
@@ -54,9 +73,14 @@ const setAria=()=>{
   display: flex;
 }
 .body{
-  margin: auto;
+  margin: 0 auto;
   width: 1000px;
   padding-bottom: 50px;
+}
+@media screen and (max-width: 1040px) {
+  .body{
+    width: calc(100vw - 40px);
+  }
 }
 .titlebar{
   top: 0;
