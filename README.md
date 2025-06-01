1d290f2a8e2c7102025f171f0f17ef4c1f3bbc6d
zhoucheng <zhoucheng133@outlook.com>
2025-06-01 19:26:09 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Jackett Helper Web
 
+![License](https://img.shields.io/badge/License-MIT-dark_green)
+
 主仓库[在这里](https://github.com/Zhoucheng133/Jackett-Helper)
--- src/env/static.ts
+++ src/env/static.ts
@@ -1,2 +1,2 @@
-// export const hostname="";
-export const hostname="http://127.0.0.1:3000";
+export const hostname="";
+// export const hostname="http://127.0.0.1:3000";
